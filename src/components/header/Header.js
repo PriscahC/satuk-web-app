721f02336e98f0c6e0aa7c02f4975982a64a6bd1
Melvinoh <murichumelvin@gmail.com>
2023-04-21 21:40:30 +0300

    changeson css files

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,35 @@
-
 import './header.css'
 import { TbBellRinging2Filled } from 'react-icons/tb'
 import {MdMessage} from "react-icons/md"
 import {FaBars} from "react-icons/fa"
-
+import { useState } from 'react'
 
 
 const Header = () => {
+    const [openNav , setopenNav] = useState();
+    const navOPen = ()=>{
+        setopenNav(true);
+    }
+    const closenav = ()=>{
+        setopenNav(false);
+    }
+
   return ( 
     <div className='container'>
         <div className="logo">
-            <span className="title">
+            <h3 className="title">
                 SATUK
-            </span>
+            </h3>
             <span className="name">
                 student association of TUK
             </span>
         </div>
         <div className="icons_container">
-            <FaBars className='icons  bars'/>
+            <FaBars className='icons  bars' onClick={navOPen}/>
             <TbBellRinging2Filled className='icons'/>
             <MdMessage className='icons'/>
-            <img src="/home/muturi/desktop/satuk_app/public/pictures/pexels-godisable-jacob-718978.jpg" alt="profile" className='profile_pic' />
         </div>
+       
     </div>
   )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,37 +10,39 @@ import {BsSuitClub} from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="nav_item">
-            <VscHome className='nav_icons'/>
-            <span>home</span>
+    <div className="nav_container">
+        <div className="navbar">
+            <div className="nav_item">
+                <VscHome className='nav_icons'/>
+                <span>home</span>
+            </div>
+            <div className="nav_item">
+                <MdOutlineSchool className='nav_icons'/>
+                <span>schools</span>
+            </div>
+            <div className="nav_item">
+                <BsSuitClub className='nav_icons'/>
+                <span>clubs</span>
+            </div>
+            <div className="nav_item">
+                <MdSportsTennis className='nav_icons'/>
+                <span>sports</span>
+            </div>
+            <div className="nav_item">
+                <MdOutlineShoppingBasket className='nav_icons'/>
+                <span>tuk soko</span>
+            </div>
+            <div className="nav_item">
+                <IoPeopleCircleOutline className='nav_icons'/>
+                <span>leadership</span>
+            </div>
+            
+            <div className="nav_item">
+                <GrGallery className='nav_icons'/>
+                <span>gallery</span>
+            </div>
         </div>
-        <div className="nav_item">
-            <MdOutlineSchool className='nav_icons'/>
-            <span>schools</span>
-        </div>
-        <div className="nav_item">
-            <BsSuitClub className='nav_icons'/>
-            <span>clubs</span>
-        </div>
-        <div className="nav_item">
-            <MdSportsTennis className='nav_icons'/>
-            <span>sports</span>
-        </div>
-        <div className="nav_item">
-            <MdOutlineShoppingBasket className='nav_icons'/>
-            <span>tuk soko</span>
-        </div>
-        <div className="nav_item">
-            <IoPeopleCircleOutline className='nav_icons'/>
-            <span>leadership</span>
-        </div>
-        
-        <div className="nav_item">
-            <GrGallery className='nav_icons'/>
-            <span>gallery</span>
-        </div>
-    </div>
+    </div>    
   )
 }
 
