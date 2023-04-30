90242a9e6b8f9ad88b3b19e4eca040308844468c
Melvinoh <murichumelvin@gmail.com>
2023-04-30 22:39:59 +0300

    react app

--- src/components/slides/SliderWrapper.js
+++ src/components/slides/SliderWrapper.js
@@ -1,37 +1,21 @@
 import React from 'react'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./slider.css";
-//import Slidedata from '../../data/Slidedata'
-//import Slidecard from '../card_slide/Slidecard';
+import { Carousel } from 'react-responsive-carousel';
 
 function SliderWrapper() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-    };
-   
   return (
-    <Slider {...settings}  >
-      <div>
-        <h1>1</h1>
+    <Carousel autoPlay infiniteLoop showArrows={false} dots={true} className='wrapper'>
+      <div className='slides'>
+        <h3>Slide 1</h3>
       </div>
-      <div>
-      <h1>2</h1>
+      <div  className='slides'>
+        <h3>Slide 2</h3>
       </div>
-      <div>
-      <h1>3</h1>
+      <div  className='slides'>
+        <h3>Slide 3</h3>
       </div>
-      <div>
-      <h1>4</h1>
-      </div>
-
-    </Slider>
-  )
+    </Carousel>
+  );
 }
+
 
 export default SliderWrapper
